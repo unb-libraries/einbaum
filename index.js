557f5e9dc5eb68ed8d49404697d11678f9b80d06
Patrick Schilf <pschilf@unb.ca>
2022-12-14 11:54:34 -0400

    Expose plugin symlinking method

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+
 const defaultConfig = require('./einbaum.config.default')
 const merge = require('lodash.merge')
 
+const PLUGIN_TYPES = [
+  'commands',
+  'preprocessors',
+  'selectors',
+  'workflows',
+]
+
+const symlinkPluginFiles = (plugins, targetRoot) => {
+  const symlinks = {}
+  plugins.forEach(plugin => {
+    const pluginRoot = path.dirname(require.resolve(plugin))
+    PLUGIN_TYPES.forEach(type => {
+      symlinks[type] = []
+      const pluginTypeRoot = path.resolve(pluginRoot, type)
+      if (fs.existsSync(pluginTypeRoot)) {
+        const targetTypeRoot = path.resolve(targetRoot, type)
+        if (!fs.existsSync(targetTypeRoot)) {
+          fs.mkdirSync(targetTypeRoot)
+        }
+        fs.readdirSync(pluginTypeRoot).forEach(filename => {
+          const targetPath = path.resolve(targetTypeRoot, filename)
+          const pluginPath = path.resolve(pluginTypeRoot, filename)
+          symlinks[type].push(targetPath)
+          if (!fs.existsSync(targetPath)) {
+            fs.symlinkSync(pluginPath, targetPath)
+          }
+        })
+      }
+    })
+  })
+  return symlinks
+}
+
 module.exports = {
   defineConfig: (config) => merge(defaultConfig, config),
+  symlinkPluginFiles,
 }
