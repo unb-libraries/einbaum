348869dae5c39b12866e0e9f8d724f9c7aabce96
Patrick Schilf <pschilf@unb.ca>
2023-01-03 13:21:16 -0400

    Enable overriding config by ENV variables

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const defaultConfig = require('./einbaum.config.default')
 const merge = require('lodash.merge')
+const snakeCase = require('lodash.snakecase')
+
+const defineConfig = (config) => {
+  config = merge(defaultConfig, config)
+  Object.keys(config).forEach(key => {
+    const snakeKey = snakeCase(key).toUpperCase()
+    if (process.env[snakeKey]) {
+      config[key] = process.env[snakeKey]
+    }
+  })
+  return config
+}
 
 module.exports = {
-  defineConfig: (config) => merge(defaultConfig, config),
+  defineConfig,
 }
