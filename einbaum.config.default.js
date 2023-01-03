e82bea550b45a45f371e72aca80e413a22e3292e
Patrick Schilf <pschilf@unb.ca>
2023-01-03 13:40:45 -0400

    Revert default "baseUrl" config

--- einbaum.config.default.js
+++ einbaum.config.default.js
@@ -1,6 +1,6 @@
 const { PROJECT_ROOT } = process.env
 module.exports = {
-  baseUrl: process.env.BASE_URL || "http://localhost:8080",
+  baseUrl: "http://localhost:8080",
   fixturesFolder: `${PROJECT_ROOT}/fixtures`,
   plugins: {},
 }
