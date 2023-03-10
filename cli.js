48314d8c5c17953ccdd5365a5ba9c6a2221e1c81
Patrick Schilf <pschilf@unb.ca>
2023-03-10 16:42:31 -0400

    Enable overriding cypress config via cli params

--- cli.js
+++ cli.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 
+const snakeCase = require('lodash.snakecase')
+
 const headless = process.argv.includes('--headless')
+process.argv
+  .filter(arg => arg.startsWith('--set-'))
+  .map(arg => arg.substring(6))
+  .forEach(arg => {
+    const [name, value] = arg.split('=')
+    const envVarName = snakeCase(name).toUpperCase()
+    if (!process.env[envVarName]) {
+      process.env[envVarName] = value
+    }
+  })
 
 const { open, run } = require("cypress")
 const path = require('path')
